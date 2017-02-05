4abcfc07fef03a709baf4b7c8d0957e5faae63bb
katynoellescott <katyscott22@gmail.com>
2017-02-05 13:40:03 -0800

    pseudocode for Whale Trail mini project

---
+++ project1.py
@@ -0,0 +1,48 @@
+#Title page with whale image
+
+#VISUALS
+#whale traveling through waters at top of every page
+#health, speed, hunger displayed on each page
+#page changes with every input option
+#for new page, use clear function
+
+#INTRO
+# raw_input() Play, quit or instructions
+# Instructions: background on whale migration; you are female whale traveling north
+# to give birth
+
+#NAMES
+# raw_input("") to enter your name, name of calf, name of friend and name of
+# friend's calf
+
+#PREPARE FOR JOURNEY
+# raw_input for how long to stay and eat and rest, to build up fat store for journey
+# and to ensure calf is ready for journey
+#function to translate this into an integer
+#function uses this integer as one input to determine health throughout journey
+
+#CONSTANT TRAVELING OPTIONS
+#function with while loop to return to traveling option page at any time
+# raw_input for how fast to travel -- whales travel 3-5 mph, but travel 100 miles
+# per day bc travel 24 hours
+
+#INTERMITTENT TRAVELING OPTIONS
+#route choices at key points, based on location, based on speed and time passed
+#weather choices -- random
+#stop to eat
+#join other pods
+#communicate with other whales
+
+#HEALTH function
+#health increase with more food
+#health decreases with less food, plastic gyre, fights with orcas, whalers
+#health decreases with fast pace
+#health increases with communication with other whales, joining other pods
+
+#HAZARDS
+#function to determine which hazards happen when -- based on traveling options
+#hazards include plastic gyre, killer whales, weather, whalers, starving and
+#illness
+
+#GOAL
+#make it to northern feeding grounds
